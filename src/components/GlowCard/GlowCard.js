0472caabd3b7e150bfdae315c2a1582c889ef9d8
paarshan4800 <paargav.shuttle@gmail.com>
2021-02-20 09:07:28 +0530

    rectified master branch in remote

---
+++ src/components/GlowCard/GlowCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './GlowCard.module.css'
+import FadeInSection from '../FadeInSection/FadeInSection'
+
+/*
+Usage:
+<GlowCard props={{title:"Boom", content:"This is Everything"}}
+*/
+function GlowCard({props}) {
+    return (
+        <FadeInSection>
+            <div className={styles.bg}>
+                <div className={styles.box}>
+                    <div className={styles.glass}></div>
+                    <div className={styles.content}>
+                        <h2>{props.title}</h2>
+                        <br/>
+                        <p>{props.content}</p>
+                    </div>
+                </div>
+            </div>
+        </FadeInSection>
+    )
+}
+
+export default GlowCard
