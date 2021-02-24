dc032e228b21f2b487b3c3c373eab467214775c0
Umar Ahmed T <tumarahmed@gmail.com>
2021-02-24 18:37:02 +0530

    Scroll bar added

--- src/pages/LoginRegister/Container/SignIn/SignIn.jsx
+++ src/pages/LoginRegister/Container/SignIn/SignIn.jsx
@@ -7,10 +7,15 @@ const SignIn = ({ children }) => (
         {/* <h1 className="login-register-heading">Sign In Form</h1> */}
         <Heading text="LOGIN" fontSize="35px"></Heading>
         <div className="form-class">
+            {/* <div>
+        	    <input class="effect-5" type="text" placeholder="Placeholder Text" />
+                <span class="focus-border"></span>
+            </div> */}
             <input type="text" placeholder="Email" required />
             <input type="password" placeholder="Password" required />
             {/* <div></div> - Validation Output */}
         </div>
+        <a href=""><div className="forgot-password">Forgot password?</div></a>
         <NeonButton props={{text:"Sign In", href:"/events", color:"#26a0da"}}/>
     </div>
 );
