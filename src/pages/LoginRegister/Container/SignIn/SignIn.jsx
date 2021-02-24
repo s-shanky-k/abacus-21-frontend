d9d91ca35f38fe3e0fc4e1478d924fe52c2693f1
paarshan4800 <paargav.shuttle@gmail.com>
2021-02-25 00:44:28 +0530

    login and register integrated

--- src/pages/LoginRegister/Container/SignIn/SignIn.jsx
+++ src/pages/LoginRegister/Container/SignIn/SignIn.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Heading from '../../../../components/Heading/Heading.js';
 import NeonButton from '../../../../components/NeonButton/NeonButton.js';
+import { apiSignin } from "../../../../api/api";
+import { Link } from "react-router-dom"
 
-const SignIn = ({ children }) => (
-    <div className="form-container sign-in-container">
-        {/* <h1 className="login-register-heading">Sign In Form</h1> */}
-        <Heading text="LOGIN" fontSize="35px"></Heading>
-        <div className="form-class">
-            {/* <div>
-        	    <input class="effect-5" type="text" placeholder="Placeholder Text" />
-                <span class="focus-border"></span>
-            </div> */}
-            <input type="text" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            {/* <div></div> - Validation Output */}
+const url_signin = "ec2-3-16-135-186.us-east-2.compute.amazonaws.com:3000/auth/signin"
+
+
+
+const SignIn = ({ children }) => {
+    const [email, setemail] = useState('')
+    const [pwd, setpwd] = useState('')
+    return (
+        <div className="form-container sign-in-container">
+            <Heading text="LOGIN" fontSize="35px"></Heading>
+            <div className="form-class">
+                <input type="text" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)} />
+                <input type="password" placeholder="Password" required value={pwd} onChange={(e) => setpwd(e.target.value)} />
+                {/* <div></div> - Validation Output */}
+            </div>
+            <Link href=""><div className="forgot-password">Forgot password?</div></Link>
+            <NeonButton props={{ text: "Sign In", color: "#26a0da", onClick: apiSignin, credentials: { email: email, pwd: pwd } }} />
         </div>
-        <a href=""><div className="forgot-password">Forgot password?</div></a>
-        <NeonButton props={{text:"Sign In", href:"/events", color:"#26a0da"}}/>
-    </div>
-);
+    )
+}
 
 export default SignIn;
