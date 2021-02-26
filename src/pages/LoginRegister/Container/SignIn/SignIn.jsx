abe25428b2a20c89cc135df2902e011e61946394
Umar Ahmed T <tumarahmed@gmail.com>
2021-02-26 12:46:48 +0530

    Funtional component updated to class components

--- src/pages/LoginRegister/Container/SignIn/SignIn.jsx
+++ src/pages/LoginRegister/Container/SignIn/SignIn.jsx
@@ -1,28 +1,103 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import Heading from '../../../../components/Heading/Heading.js';
 import NeonButton from '../../../../components/NeonButton/NeonButton.js';
 import { apiSignin } from "../../../../api/api";
 import { Link } from "react-router-dom"
 
-const url_signin = "ec2-3-16-135-186.us-east-2.compute.amazonaws.com:3000/auth/signin"
+const url_signin = "ec2-3-16-135-186.us-east-2.compute.amazonaws.com:3000/auth/signin";
+
+const initialState = {
+    email : '',
+    pwd : '',
+    emailError:'',
+    pwdError:'',
+}
 
 
+// const SignIn = ({ children }) => {
+//     const [email, setemail] = useState('')
+//     const [pwd, setpwd] = useState('')
+//     return (
+//         <div className="form-container sign-in-container">
+//             <Heading text="LOGIN" fontSize="35px"></Heading>
+//             <div className="form-class">
+//                 <input type="text" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)} />
+//                 <input type="password" placeholder="Password" required value={pwd} onChange={(e) => setpwd(e.target.value)} />
+//                 {/* <div></div> - Validation Output */}
+//             </div>
+//             <Link href=""><div className="forgot-password">Forgot password?</div></Link>
+//             <NeonButton props={{ text: "Sign In", color: "#26a0da", onClick: apiSignin, credentials: { email: email, pwd: pwd } }} />
+//         </div>
+//     )
+// }
 
-const SignIn = ({ children }) => {
-    const [email, setemail] = useState('')
-    const [pwd, setpwd] = useState('')
-    return (
+export default class SignIn extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+        this.textInput = React.createRef();
+    }
+
+    validate = () => {
+        let emailError = "";
+
+        if(!this.state.pwd) {
+            emailError = 'Password field cannot be blank';
+        }
+        
+        if(!this.state.email.includes('@')) {
+            emailError = 'Invalid Email! Try a different one!';
+        }
+
+        if(!this.state.email) {
+            emailError = 'Email field cannot be blank';
+        }
+
+        if(emailError) {
+            this.setState({emailError});
+            return false;
+        }
+
+        return true;
+    }
+
+    handleSubmit = () => {
+        const isValid = this.validate();
+        if(isValid) {
+            console.log(this.state);
+            this.setState(initialState);
+        }
+    }
+
+    onSubmit = () => {
+        this.handleSubmit();
+        // apiSignin();
+    }
+
+    componentDidMount() {
+        this.textInput.current.focus();
+    }
+
+    giveFocus = () => {
+        this.textInput.current.focus();
+    }
+
+    render() {
+        return (
         <div className="form-container sign-in-container">
             <Heading text="LOGIN" fontSize="35px"></Heading>
             <div className="form-class">
-                <input type="text" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)} />
-                <input type="password" placeholder="Password" required value={pwd} onChange={(e) => setpwd(e.target.value)} />
-                {/* <div></div> - Validation Output */}
+                <input ref={this.textInput} type="text" placeholder="Email" required value={this.state.email} onChange={(e) => this.setState({email : e.target.value})} />
+                <input type="password" placeholder="Password" required value={this.state.pwd} onChange={(e) => this.setState({pwd : e.target.value})} />
             </div>
+            {this.state.emailError ? (<div className="validation-output">{this.state.emailError}</div>) : null}
+            {this.state.pwdError ? (<div className="validation-output">{this.state.pwdError}</div>) : null}
             <Link href=""><div className="forgot-password">Forgot password?</div></Link>
-            <NeonButton props={{ text: "Sign In", color: "#26a0da", onClick: apiSignin, credentials: { email: email, pwd: pwd } }} />
+            <NeonButton props={{ text: "Sign In", color: "#26a0da", onClick: this.onSubmit, credentials: { email: this.state.email, pwd: this.state.pwd } }} />
         </div>
     )
+    }
 }
 
-export default SignIn;
+// export default SignIn;
--- src/pages/LoginRegister/Container/SignUp/SignUp.jsx
+++ src/pages/LoginRegister/Container/SignUp/SignUp.jsx
@@ -1,39 +1,96 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import Heading from '../../../../components/Heading/Heading.js';
 import NeonButton from '../../../../components/NeonButton/NeonButton.js';
 import { apiRegister } from "../../../../api/api";
 
-const SignUp = ({ children }) => {
+// const SignUp = ({ children }) => {
 
-    const [name, setname] = useState('')
-    const [year, setyear] = useState('')
-    const [dept, setdept] = useState('')
-    const [college, setcollege] = useState('')
-    const [email, setemail] = useState('')
-    const [phone, setphone] = useState('')
-    const [pwd, setpwd] = useState('')
-    const [cpwd, setcpwd] = useState('')
+//     const [name, setname] = useState('')
+//     const [year, setyear] = useState('')
+//     const [dept, setdept] = useState('')
+//     const [college, setcollege] = useState('')
+//     const [email, setemail] = useState('')
+//     const [phone, setphone] = useState('')
+//     const [pwd, setpwd] = useState('')
+//     const [cpwd, setcpwd] = useState('')
 
-    return (
-        <div className="form-container sign-up-container">
-            {/* <h1 className="login-register-heading">Sign Up Form</h1> */}
-            <Heading text="REGISTER" fontSize="35px"></Heading>
-            <div className="form-class align-form" id="style-2">
-                <input type="text" placeholder="Name" required value={name} onChange={(e) => setname(e.target.value)} />
-                <input type="text" placeholder="Year" required value={year} onChange={(e) => setyear(e.target.value)} />
-                <input type="text" placeholder="Department" required value={dept} onChange={(e) => setdept(e.target.value)} />
-                <input type="text" placeholder="College" required value={college} onChange={(e) => setcollege(e.target.value)} />
-                {/* <input type="text" placeholder="City" required />
-                <input type="text" placeholder="State" required /> */}
-                <input type="email" placeholder="Email-ID" required value={email} onChange={(e) => setemail(e.target.value)} />
-                <input type="number" placeholder="Phone" required value={phone} onChange={(e) => setphone(e.target.value)} />
-                <input type="password" placeholder="Password" required value={pwd} onChange={(e) => setpwd(e.target.value)} />
-                <input type="password" placeholder="Confirm Password" required value={cpwd} onChange={(e) => setcpwd(e.target.value)} />
-            </div>
-            {/* <div></div> - Validation Output */}
-            <NeonButton props={{ text: "Sign Up", color: "#26a0da", onClick: apiRegister, credentials: { name: name, year: year, dept: dept, college: college, email: email, phone: phone, pwd: pwd, cpwd: cpwd } }} />
-        </div>
-    );
+//     return (
+//         <div className="form-container sign-up-container">
+//             {/* <h1 className="login-register-heading">Sign Up Form</h1> */}
+//             <Heading text="REGISTER" fontSize="35px"></Heading>
+//             <div className="form-class align-form" id="style-2">
+//                 <input type="text" placeholder="Name" required value={name} onChange={(e) => setname(e.target.value)} />
+//                 <input type="text" placeholder="Year" required value={year} onChange={(e) => setyear(e.target.value)} />
+//                 <input type="text" placeholder="Department" required value={dept} onChange={(e) => setdept(e.target.value)} />
+//                 <input type="text" placeholder="College" required value={college} onChange={(e) => setcollege(e.target.value)} />
+//                 {/* <input type="text" placeholder="City" required />
+//                 <input type="text" placeholder="State" required /> */}
+//                 <input type="email" placeholder="Email-ID" required value={email} onChange={(e) => setemail(e.target.value)} />
+//                 <input type="number" placeholder="Phone" required value={phone} onChange={(e) => setphone(e.target.value)} />
+//                 <input type="password" placeholder="Password" required value={pwd} onChange={(e) => setpwd(e.target.value)} />
+//                 <input type="password" placeholder="Confirm Password" required value={cpwd} onChange={(e) => setcpwd(e.target.value)} />
+//             </div>
+//             {/* <div></div> - Validation Output */}
+//             <NeonButton props={{ text: "Sign Up", color: "#26a0da", onClick: apiRegister, credentials: { name: name, year: year, dept: dept, college: college, email: email, phone: phone, pwd: pwd, cpwd: cpwd } }} />
+//         </div>
+//     );
+// }
+
+export default class SignUp extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name : '',
+            year : '',
+            dept : '',
+            college : '',
+            email : '',
+            phone : '',
+            pwd : '',
+            cpwd : '',
+            nameError : '',
+            yearError : '',
+            deptError : '',
+            collegeError : '',
+            emailError : '',
+            phoneError : '',
+            pwdError : '',
+            cpwdError : '',
+        };
+        this.textInput = React.createRef();
+    }
+
+    // componentDidMount() {
+    //     this.textInput.current.focus();
+    // }
+
+    giveFocus() {
+        this.textInput.current.focus();
+    }
+
+    render() {
+        return (
+            <div className="form-container sign-up-container">
+                {/* <h1 className="login-register-heading">Sign Up Form</h1> */}
+                <Heading text="REGISTER" fontSize="35px"></Heading>
+                <div className="form-class align-form" id="style-2">
+                    <input ref={this.textInput} type="text" placeholder="Name" required value={this.state.name} onChange={(e) => this.setState({name : e.target.value})} />
+                    <input type="text" placeholder="Year" required value={this.state.year} onChange={(e) => this.setState({year : e.target.value})} />
+                    <input type="text" placeholder="Department" required value={this.state.dept} onChange={(e) => this.setState({dept : e.target.value})} />
+                    <input type="text" placeholder="College" required value={this.state.college} onChange={(e) => this.setState({college : e.target.value})} />
+                    {/* <input type="text" placeholder="City" required />
+                    <input type="text" placeholder="State" required /> */}
+                    <input type="email" placeholder="Email-ID" required value={this.state.email} onChange={(e) => this.setState({email : e.target.value})} />
+                    <input type="number" placeholder="Phone" required value={this.state.phone} onChange={(e) => this.setState({phone : e.target.value})} />
+                    <input type="password" placeholder="Password" required value={this.state.pwd} onChange={(e) => this.setState({pwd : e.target.value})} />
+                    <input type="password" placeholder="Confirm Password" required value={this.state.cpwd} onChange={(e) => this.setState({cpwd : e.target.value})} />
+                </div>
+                <div>{this.state.name}</div>
+                <NeonButton props={{ text: "Sign Up", color: "#26a0da", onClick: apiRegister, credentials: { name: this.state.name, year: this.state.year, dept: this.state.dept, college: this.state.college, email: this.state.email, phone: this.state.phone, pwd: this.state.pwd, cpwd: this.state.cpwd } }} />
+            </div>    
+        );
+    }
 }
 
-export default SignUp;
+// export default SignUp;
