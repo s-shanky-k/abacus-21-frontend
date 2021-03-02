fd88dd475e9873e3e757daf8d0a4a2686decd432
Darrshana <darrshana17@gmail.com>
2021-03-02 14:04:38 +0530

    devs section added

--- src/components/Dev/Dev.js
+++ src/components/Dev/Dev.js
@@ -7,15 +7,16 @@ import FadeInSection from '../FadeInSection/FadeInSection'
 function Dev({ props }){
             return (
                 <>
-        <FadeInSection></FadeInSection>    
-        <div className= {`${styles.inner}`} >
+        <FadeInSection></FadeInSection> 
+      <div style={{justifyContent: 'center',alignItems: 'center'}}>
+        <div className= {`${styles.inner}`}>
            <img src={`${process.env.PUBLIC_URL}/images/` + `${props.img}`} alt="" style={{ borderRadius: '100%' ,
-        width:'100%',height:'100%' }} />
-          </div>
-          <div style={{ paddingTop: '30px' ,textAlign:'center'}}>
-          <Heading1 text={props.title} fontSize="18px" />
-          <Heading1 text={props.title1} fontSize="15px" />
-    </div>
+        width:'100%',height:'100%' }} /></div>
+    <a href={`${props.href}`}style={{textDecoration:'none'}}><p className={`${styles.text}`}>{props.title}</p></a>
+        <p className={`${styles.text1}`}>{props.title1}</p>
+        </div>
+          
+       
 </>
         )
     }
