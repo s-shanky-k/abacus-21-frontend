2e466459164218715c464bdb33e5018cd6b09ccf
paarshan4800 <paargav.shuttle@gmail.com>
2021-03-06 21:51:04 +0530

    updated workshop data

--- src/assets/Data/WorkshopsData.js
+++ src/assets/Data/WorkshopsData.js
@@ -9,25 +9,25 @@ export const Workshops = {
             "Yet to Decide"
         ],
 
-        speakers : [
+        speakers: [
             {
-                name:"Paargav",
-                profession:"xyz"
+                name: "Paargav",
+                profession: "xyz"
             },
             {
-                name:"Paargav",
-                profession:"xyz"
+                name: "Paargav",
+                profession: "xyz"
             },
             {
-                name:"Paargav",
-                profession:"xyz"
+                name: "Paargav",
+                profession: "xyz"
             },
             {
-                name:"Paargav",
-                profession:"xyz"
+                name: "Paargav",
+                profession: "xyz"
             }
         ],
-        sponsor:"",
+        sponsor: "",
         contact: [
             {
                 name: "Guru Naveen K",
@@ -74,9 +74,9 @@ export const Workshops = {
     "cyber-security": {
         name: "Cyber Security",
         refName: "cyber-security",
-        
-        /* about: `Are you looking for a bizarre event that furnishes a crazy time?. Then get ready and jump in. This is an event for all the non technical pros out there.`,
 
+        about: `Are you looking for a bizarre event that furnishes a crazy time?. Then get ready and jump in. This is an event for all the non technical pros out there.`,
+        /*
         dateTime: [
 
         ],
@@ -129,48 +129,48 @@ export const Workshops = {
         refName: "cloud-computing",
         about: `Are you looking for a bizarre event that furnishes a crazy time?. Then get ready and jump in. This is an event for all the non technical pros out there.`,
 
-       /*  dateTime: [
-
-        ],
-
-        contact: [
-            {
-                name: "Guru Naveen K",
-                mobile: "6383357656"
-            },
-            {
-                name: "Muralikrishnan S",
-                mobile: "9092432578"
-            },
-            {
-                name: "Surya N",
-                mobile: "9500372299"
-            }
-        ],
-
-        sponsor: "",
-
-        rules: [
-            "This event is for individual participant only",
-            "Member involved in any kind of malpractice will be disqualified immediately",
-            "Decisions made by the administrators will be final",
-            "Participants must ensure that they have laptop with good internet connectivity for their convenience",
-            "Participants must ensure that they have microsoft account"
-        ],
-
-        rounds: {
-            number: 2,
-            roundDetails: [
-                {
-                    title: "Prelims",
-                    description: "Participants will be given 30 minutes to answer questions from familiar topics."
-                },
-                {
-                    title: "Finals",
-                    description: "A round in round robin fashion. Based on Kollywood (60 minutes)"
-                },
-            ]
-        }, */
+        /*  dateTime: [
+ 
+         ],
+ 
+         contact: [
+             {
+                 name: "Guru Naveen K",
+                 mobile: "6383357656"
+             },
+             {
+                 name: "Muralikrishnan S",
+                 mobile: "9092432578"
+             },
+             {
+                 name: "Surya N",
+                 mobile: "9500372299"
+             }
+         ],
+ 
+         sponsor: "",
+ 
+         rules: [
+             "This event is for individual participant only",
+             "Member involved in any kind of malpractice will be disqualified immediately",
+             "Decisions made by the administrators will be final",
+             "Participants must ensure that they have laptop with good internet connectivity for their convenience",
+             "Participants must ensure that they have microsoft account"
+         ],
+ 
+         rounds: {
+             number: 2,
+             roundDetails: [
+                 {
+                     title: "Prelims",
+                     description: "Participants will be given 30 minutes to answer questions from familiar topics."
+                 },
+                 {
+                     title: "Finals",
+                     description: "A round in round robin fashion. Based on Kollywood (60 minutes)"
+                 },
+             ]
+         }, */
         registration: true,
         purpose: "CLOUD"
     },
