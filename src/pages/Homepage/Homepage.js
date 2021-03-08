23c0ada28c94ce991c08425d796c3508381c147c
rampage-rider <seshankar20@gmail.com>
2021-03-08 21:57:54 +0530

    Fixed homepage layout

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -16,6 +16,7 @@ import DashboardCard from "../../components/DashboardCard/DashboardCard";
 import { Autocomplete } from "@material-ui/lab";
 import FadeInSection from "../../components/FadeInSection/FadeInSection";
 import GlowCardSimple from "../../components/GlowCardSimple/GlowCardSimple";
+import { Fade } from "@material-ui/core";
 
 function demoAsyncCall() {
   return new Promise((resolve) => setTimeout(() => resolve(), 2000));
@@ -45,18 +46,16 @@ class Homepage extends Component {
         <section id="#">
           <div
             className={`${styles._homepage} ${styles._responsive} ${styles.bg1}`}
-          > 
+          >
             <img
               src={`${process.env.PUBLIC_URL}/images/about-us/abacus logo sponsors page.png`}
               alt=""
               style={{
                 width: "330px",
                 height: "350px",
-                margin:"auto"
-                
+                margin: "auto",
               }}
             />
-           
             <div className="container-fluid  ml-sm-0  justify-content-center align-self-center pb-5">
               <div className="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-xs-2 justify-content-center align-self-center ">
                 <div className=" col mt-5   text-center">
@@ -111,7 +110,6 @@ class Homepage extends Component {
             </div>{" "}
           </div>
         </section>
-     
 
         {/* Child Class */}
         {/* <div className={`${styles._homepage} ${styles._responsive} ${styles.bg}`}>
@@ -125,87 +123,95 @@ class Homepage extends Component {
 </div> </FadeInSection>
                </div>
                <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-               <GlowCardSimple props={{degree: "25deg",title:"Abacus'21",content:"CSEA takes immense pleasure in organizing Abacus’21. This version is going to be one of its kind - a symposium completely organized online. We operate with a vision – to bring together students from multiple engineering colleges globally and give them a chance to demonstrate their technical prowess, ignite their passion for learning and to win exciting prizes and internships. Grab this opportunity and win cash prizes, Intern Opportunities , T shirts and many surprising prizes."}} />    
+               <GlowCard props={{degree: "25deg",title:"Abacus'21",content:"CSEA takes immense pleasure in organizing Abacus’21. This version is going to be one of its kind - a symposium completely organized online. We operate with a vision – to bring together students from multiple engineering colleges globally and give them a chance to demonstrate their technical prowess, ignite their passion for learning and to win exciting prizes and internships. Grab this opportunity and win cash prizes, Intern Opportunities , T shirts and many surprising prizes."}} />    
                  </div>
              </div>
            </div>
             </div> */}
 
-          <div
-            className={`${styles._homepage} ${styles.bg}`}
-
-          >
-            <div className={`${styles._about_event}`}>
-              <div className={`${styles._glowCardDiv}`}>
-                <GlowCard props={{
+        <div className={`${styles._homepage} ${styles.bg}`}>
+          <div className={`${styles._about_event}`}>
+            <div className={`${styles._glowCardDiv}`}>
+              <GlowCard
+                props={{
                   title: "Abacus'21",
-                  /* content:"CSEA takes immense pleasure in organizing Abacus’21. This version is going to be one of its kind - a symposium completely organized online. We operate with a vision – to bring together students from multiple engineering colleges globally and give them a chance to demonstrate their technical prowess, ignite their passion for learning and to win exciting prizes and internships. Grab this opportunity and win cash prizes, Intern Opportunities , T shirts and many surprising prizes."
-                   */list:[
+                  list: [
                     "CSEA takes immense pleasure in organizing Abacus’21. This version is going to be one of its kind - a symposium completely online. We operate with a vision – to bring together students from multiple engineering colleges globally and give them a chance to demonstrate their technical prowess, ignite their passion for learning and to win exciting prizes and internships.",
-                    "Grab this opportunity and win cash prizes, Intern opportunities, Tshirts and many more prizes."
-                  ]
-                }} />
-              </div>
-              <div className={`${styles._eventCardImgDiv}`}>
-                <div className="embed-responsive embed-responsive-21by9" className={styles.eventCardImg}>
-                    <iframe src="https://www.youtube.com/embed/W0eDjWr3ATI" width = "100%" height="65%" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                {/* <img src={`${process.env.PUBLIC_URL}/images/events/` + `${props.refName}` + `.svg`} alt="" className={styles.eventCardImg} /> */}
-              </div>
+                    "Grab this opportunity and win cash prizes, Intern opportunities, Tshirts and many more prizes.",
+                  ],
+                }}
+              />
             </div>
-          </div>
             
-
-
-
-              
-              
-              
-              
-          
+            <div className={`${styles._eventCardImgDiv}`}>
+            
+              <div
+                className="embed-responsive embed-responsive-21by9"
+                className={styles.eventCardImg}
+              >
+                
+                <iframe
+                  src="https://www.youtube.com/embed/W0eDjWr3ATI"
+                  width="100%"
+                  height="65%"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                >
+                </iframe>
+                
+              </div>
+              </div>
+          </div>
+        </div>
 
         {/* Child Class */}
-      <div className={`${styles._homepage} ${styles.bg1} ${styles._responsive}`}>
-      <div className="text-center">
+        <div
+          className={`${styles._homepage} ${styles.bg1} ${styles._responsive}`}
+        >
+          <div className="text-center">
             <Heading text="Sponsors" fontSize="60px" />
             <div class="mt-5">
-            <Heading text="Title Sponsors" fontSize="30px" />
+              <Heading text="Title Sponsors" fontSize="30px" />
             </div>
           </div>
-          
+
           <div className="container-fluid mx-auto d-flex justify-content-center align-self-center mt-0">
             <div className="row row-cols-lg-2 row-cols-md-2 row-cols-sm-2 row-cols-xs-1  d-flex justify-content-center align-self-center">
               <div className="col text-center pr-lg-5 ">
-               
-              <img src={`${process.env.PUBLIC_URL}/images/about-us/motorq.svg`}  className={`${styles.img}`}  
-              alt="Motorq"/></div>
-              
+                <FadeInSection>
+                  <img
+                    src={`${process.env.PUBLIC_URL}/images/about-us/motorq.svg`}
+                    className={`${styles.img}`}
+                    alt="Motorq"
+                  />
+                </FadeInSection>
+              </div>
+
               <div className="col text-center pl-lg-5">
-              <img src={`${process.env.PUBLIC_URL}/images/about-us/visa_modified.svg`} className={`${styles.img}`} alt="Visa"/>
-              </div></div></div>
-          
-           
+                <FadeInSection>
+                  <img
+                    src={`${process.env.PUBLIC_URL}/images/about-us/visa_modified.svg`}
+                    className={`${styles.img}`}
+                    alt="Visa"
+                  />
+                </FadeInSection>
+              </div>
             </div>
-
-
-               
-          
-          
-          
-            
-           
-        
+          </div>
+        </div>
 
         {/* Child Class */}
-        <div className={`${styles._homepage} ${styles._responsive} ${styles.bg}`}>
+        <div
+          className={`${styles._homepage} ${styles._responsive} ${styles.bg}`}
+        >
           <div className="text-center mt-5">
             <Heading text="Developers and Design Team" fontSize="40px" />
           </div>
           <div className="container-fluid m-auto d-flex justify-content-center pt-5 align-self-center pb-5">
             <div className="row row-cols-sm-2 row-cols-md-3 row-cols-lg-5  d-flex justify-content-center align-self-center pb-5">
-              
-
               <div className="col   pb-5 ">
+              <FadeInSection>
                 <Dev
                   props={{
                     title: "Darrshana",
@@ -214,10 +220,11 @@ class Homepage extends Component {
                     href:
                       "https://www.linkedin.com/in/darrshana-rajkumar-6a36aa1ba",
                   }}
-                />
+                /> </FadeInSection>
               </div>
 
               <div className="col   pb-5 ">
+              <FadeInSection>
                 <Dev
                   props={{
                     title: "Paargav Shanker",
@@ -227,88 +234,98 @@ class Homepage extends Component {
                       "https://www.linkedin.com/in/paargav-shanker-b6708a194/",
                   }}
                 />
+                </FadeInSection>
               </div>
+              
               <div className="col  pb-5 ">
+              <FadeInSection>
                 <Dev
                   props={{
                     title: "Shankar Kumar",
                     title1: "Frontend",
                     img: "dev/shankar.jpeg",
-                    href:
-                      "https://www.linkedin.com/in/shankar-kumar-s",
+                    href: "https://www.linkedin.com/in/shankar-kumar-s",
                   }}
                 />
+                </FadeInSection>
               </div>
-              
-                
+
               <div className="col  pb-5 ">
+              <FadeInSection>
                 <Dev
                   props={{
                     title: "Srinath Sureshkumar",
                     title1: "Backend",
                     img: "dev/Srinath.jpg",
-                    href:"https://www.linkedin.com/in/srinath-sureshkumar-3467331a5/"
+                    href:
+                      "https://www.linkedin.com/in/srinath-sureshkumar-3467331a5/",
                   }}
-                />
+                /> </FadeInSection>
               </div>
               <div className="col  pb-5 ">
-              <Dev
+              <FadeInSection>
+                <Dev
                   props={{
                     title: "Umar Ahmed",
                     title1: "Frontend",
                     img: "dev/umar.jpeg",
-                    href:
-                      "https://www.linkedin.com/in/umar-ahmed-6892481b3",
+                    href: "https://www.linkedin.com/in/umar-ahmed-6892481b3",
                   }}
                 />
+              </FadeInSection>
               </div>
               <div className="col  pb-5  ">
+              <FadeInSection>
                 <Dev
                   props={{
                     title: "Adhithya",
                     title1: "Design",
                     img: "dev/Adhithya_K.jpg",
-                    href:"https://www.linkedin.com/in/adhithya-karthikeyan/"
+                    href: "https://www.linkedin.com/in/adhithya-karthikeyan/",
                   }}
                 />
+              </FadeInSection>
               </div>
               <div className="col  pb-5 ">
+              <FadeInSection>
                 <Dev
                   props={{
                     title: "Akash",
                     title1: "Design",
                     img: "dev/Akash.jpg",
-                    href:"https://www.linkedin.com/in/akash-murugesh-0151531ab/"
+                    href:
+                      "https://www.linkedin.com/in/akash-murugesh-0151531ab/",
                   }}
                 />
+                </FadeInSection>
               </div>
-              
+
               <div className="col  pb-5 ">
-                <Dev
+                <FadeInSection>
+                 <Dev
                   props={{
                     title: "Deepak",
                     title1: "Design",
                     img: "dev/deepak.jpeg",
-                    href:"https://www.linkedin.com/in/deepak24899/"
+                    href: "https://www.linkedin.com/in/deepak24899/",
                   }}
-                />
+                /> </FadeInSection>
               </div>
               <div className="col  pb-5 ">
-                <Dev
+                <FadeInSection>
+                 <Dev
                   props={{
                     title: "Rohini",
                     title1: "Design",
                     img: "dev/Rohini R.jpg",
-                    href:"https://www.linkedin.com/in/rohini-raja/"
+                    href: "https://www.linkedin.com/in/rohini-raja/",
                   }}
                 />
+                </FadeInSection>
               </div>
-              
             </div>
           </div>
         </div>
-        
-        
 
         <Footer />
       </>
