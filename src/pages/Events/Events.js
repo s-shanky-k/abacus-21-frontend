e893e55d03d405c5a3e474e72d14942d0ef1f3fc
Umar Ahmed T <tumarahmed@gmail.com>
2021-03-10 02:54:20 +0530

    Added Toast for Events and Workshops

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,45 +1,98 @@
-import React, { Component, useContext } from 'react'
+import React, { Component, useContext, useState, useEffect } from 'react'
 import styles from "./Events.module.css"
 import GlowCardResponsive from '../../components/GlowCardResponsive/GlowCardResponsive'
 import Footer from '../../components/Footer/Footer'
 import { toast } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
 import { NewLoad, SetNewLoad } from "../../App"
+import {Width} from "../../App"
 
-class Events extends Component {
+// class Events extends Component {
 
-    constructor(props) {
-        super(props)
+//     constructor(props) {
+//         super(props)
 
-        this.state = {
+//         this.state = {
+//             toastDisplay: true,
+//         }
 
+//         // this._NewLoad = React.useContext(NewLoad)
+//         // this._SetNewLoad = React.useContext(SetNewLoad)
+//     }
+
+//     componentDidMount() {
+//         // if (this._NewLoad) {
+//         //     toast.warn("Single Payment for both Tech and Non-Tech events", {
+//         //         position: toast.POSITION.BOTTOM_CENTER
+//         //     })
+//         //     this._SetNewLoad(false)
+//         // }
+
+//         if(this.state.toastDisplay) {
+//             toast.info("Single Payment for both Tech and Non-Tech Events", {
+//                 position: toast.POSITION.BOTTOM_LEFT,
+//                 autoClose: 8000,
+//                 closeButton: false,
+//                 style: customStyle,
+//             })
+//             this.state.toastDisplay = false;
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <div id="#" className={`${styles._homepage} ${styles._responsive} ${styles.bg}`} >
+//                     <GlowCardResponsive props={{ title: "Tech Events", titleFontFamily: "Iceland", href: "events/tech", img: "events/tech.svg" }} />
+//                     <GlowCardResponsive props={{ title: "Hackathon", titleFontFamily: "Iceland", href: "/hackathon", img: "events/hackathon.svg" }} />
+//                     <GlowCardResponsive props={{ title: "Non-Tech Events", titleFontFamily: "Iceland", href: "events/non-tech##", img: "events/nontech.svg" }} />
+//                 </div>
+//                 <Footer />
+//             </>
+//         )
+//     }
+// }
+
+// import React from 'react'
+
+toast.configure()
+export default function Events() {
+
+    const [toastDisplay, setToastDisplay] = useState(true)
+
+    const customStyle = {
+        display: 'flex',
+        textAlign: 'center',
+        justifyContent: 'center',
+        width: 400,
+    }
+
+    useEffect(() => {
+        if(toastDisplay) {
+            toast.info("Single Payment for both Tech and Non-Tech Events", {
+                position: toast.POSITION.BOTTOM_LEFT,
+                autoClose: 8000,
+                closeButton: false,
+                style: customStyle,
+            })
+            setToastDisplay(false);
         }
+        return () => {
+            
+        }
+    }, [])
 
-        // this._NewLoad = React.useContext(NewLoad)
-        // this._SetNewLoad = React.useContext(SetNewLoad)
-    }
-
-    componentDidMount() {
-        // if (this._NewLoad) {
-        //     toast.warn("Single Payment for both Tech and Non-Tech events", {
-        //         position: toast.POSITION.BOTTOM_CENTER
-        //     })
-        //     this._SetNewLoad(false)
-        // }
-    }
-
-    render() {
-        return (
-            <>
+    return (
+        <>
                 <div id="#" className={`${styles._homepage} ${styles._responsive} ${styles.bg}`} >
                     <GlowCardResponsive props={{ title: "Tech Events", titleFontFamily: "Iceland", href: "events/tech", img: "events/tech.svg" }} />
                     <GlowCardResponsive props={{ title: "Hackathon", titleFontFamily: "Iceland", href: "/hackathon", img: "events/hackathon.svg" }} />
                     <GlowCardResponsive props={{ title: "Non-Tech Events", titleFontFamily: "Iceland", href: "events/non-tech##", img: "events/nontech.svg" }} />
                 </div>
                 <Footer />
-            </>
-        )
-    }
+        </>
+    )
 }
 
-export default Events
+
+// export default Events
--- src/pages/Workshops/Workshops.js
+++ src/pages/Workshops/Workshops.js
@@ -1,11 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import styles from "./Workshops.module.css"
 import GlowCardResponsive from '../../components/GlowCardResponsive/GlowCardResponsive'
 import Footer from '../../components/Footer/Footer'
-class Workshops extends Component {
-    render() {
-        return (
-            <>
+import { toast } from "react-toastify"
+
+// class Workshops extends Component {
+//     render() {
+//         return (
+//             <>
+
+//                 <div id="#" className={`${styles._homepage} ${styles._responsive} ${styles.bg1}`} >
+//                     <GlowCardResponsive props={{ title: "Cloud Computing", titleFontFamily: "Iceland", content: "A cloud computing workshop", img: "workshops/cloud-computing.svg", href: "/workshops/cloud-computing" }} />
+//                     <GlowCardResponsive props={{ title: "Job Readiness", titleFontFamily: "Iceland", content: "A placement workshop", img: "workshops/job-readiness.svg", href: "/workshops/job-readiness" }} />
+//                     <GlowCardResponsive props={{ title: "Entrepreneurship", titleFontFamily: "Iceland", content: "An entrepreneurship workshop", img: "workshops/entrepreneurship.svg", href: "/workshops/entrepreneurship" }} />
+//                 </div>
+
+
+//                 <Footer />
+
+//             </>
+//         )
+//     }
+// }
+
+toast.configure()
+export default function Workshops() {
+
+    const [toastDisplay, setToastDisplay] = useState(true)
+
+    const customStyle = {
+        display: 'flex',
+        textAlign: 'center',
+        justifyContent: 'center',
+        width: 400,
+    }
+
+    useEffect(() => {
+        if(toastDisplay) {
+            toast.info("Online certificates will be provided for all Workshops", {
+                position: toast.POSITION.BOTTOM_LEFT,
+                autoClose: 8000,
+                closeButton: false,
+                style: customStyle,
+            })
+            setToastDisplay(false);
+        }
+        return () => {
+            
+        }
+    }, [])
+
+
+    return (
+        <>
 
                 <div id="#" className={`${styles._homepage} ${styles._responsive} ${styles.bg1}`} >
                     <GlowCardResponsive props={{ title: "Cloud Computing", titleFontFamily: "Iceland", content: "A cloud computing workshop", img: "workshops/cloud-computing.svg", href: "/workshops/cloud-computing" }} />
@@ -17,8 +64,8 @@ class Workshops extends Component {
                 <Footer />
 
             </>
-        )
-    }
+    )
 }
 
-export default Workshops
+
+// export default Workshops
